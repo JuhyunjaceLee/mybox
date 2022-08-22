2015b76b23d20bb98f4b9acefec3cd9386c3d2bb
Lee Juhyun <heyjace777@gmail.com>
2022-08-22 22:46:22 +0900

    home add components

---
+++ mybox_vue/src/components/HomeTvSection.vue
@@ -0,0 +1,291 @@
+<template>
+  <div>
+    <section>
+      <h2 class="section-title">MOVIE TOP 20</h2>
+      <div class="home-list-wrap">
+        <button class="prev-btn prev1"></button>
+        <ul class="home-list-container">
+          <li class="poster-list">
+            <span class="ranking">1</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">2</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">3</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">4</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">5</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">6</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">7</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">8</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">9</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">10</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <span class="ranking">11</span>
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+        </ul>
+        <button class="next-btn next1"></button>
+      </div>
+    </section>
+    <section>
+      <h2 class="section-title">NOW PLAYING</h2>
+      <div class="home-list-wrap">
+        <button class="prev-btn"></button>
+        <ul class="home-list-container">
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+        </ul>
+        <button class="next-btn"></button>
+      </div>
+    </section>
+    <section>
+      <h2 class="section-title">개봉 예정작</h2>
+      <div class="home-list-wrap">
+        <button class="prev-btn"></button>
+        <ul class="home-list-container">
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+          <li class="poster-list">
+            <a class="poster-link" href="detailPage.html">
+              <img
+                class="poster-img"
+                src="../assets/img/movie_poster.jpg"
+                alt="poster_img"
+              />
+            </a>
+          </li>
+        </ul>
+        <button class="next-btn"></button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
